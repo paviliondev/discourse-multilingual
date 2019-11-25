8110c0b892e9352c442e6e22cc6adf8d8fa29c7f
Angus McLeod <angus@mcleod.org.au>
2019-11-25 17:53:04 +1100

    Tweak tag fabrication

--- spec/components/multilingual_topic_query_spec.rb
+++ spec/components/multilingual_topic_query_spec.rb
@@ -1,15 +1,12 @@
 require 'rails_helper'
-require 'topic_view'
 
 describe TopicQuery, import_languages: true do
   context "user has content languages" do
     fab!(:user1) { Fabricate(:user) }
     fab!(:user2) { Fabricate(:user) }
     
-    fab!(:tag1) { Fabricate(:tag, name: "tag1") }
-    fab!(:tag2) { Fabricate(:tag, name: "tag2") }
-    
     fab!(:language_topic1) {
+      tag1 = Fabricate(:tag, name: "tag1")
       language_tag1 = Tag.find_by(name: 'aa')
       Fabricate(:topic, tags: [tag1, language_tag1]) 
     }
@@ -19,8 +16,14 @@ describe TopicQuery, import_languages: true do
       Fabricate(:topic, tags: [language_tag2]) 
     }
     
-    fab!(:non_language_topic1) { Fabricate(:topic, tags: [tag1]) }
-    fab!(:non_language_topic2) { Fabricate(:topic, tags: [tag2]) }
+    fab!(:non_language_topic1) { 
+      tag1 = Fabricate(:tag, name: "tag1")
+      Fabricate(:topic, tags: [tag1]) 
+    }
+    fab!(:non_language_topic2) {
+      tag2 = Fabricate(:tag, name: "tag2")
+      Fabricate(:topic, tags: [tag2]) 
+    }
     
     it "filters topic list properly" do
       user1.custom_fields['content_languages'] = ['aa', 'am']
