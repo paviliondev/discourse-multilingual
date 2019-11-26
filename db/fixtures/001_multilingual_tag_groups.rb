545a4adf9a2e6a24000f7f3c5baa0fa826b80ac9
Angus McLeod <angus@mcleod.org.au>
2019-11-26 11:18:59 +1100

    Update tag_group permissions

--- db/fixtures/001_multilingual_tag_groups.rb
+++ db/fixtures/001_multilingual_tag_groups.rb
@@ -4,10 +4,13 @@ if tag_group.blank?
   tag_group = TagGroup.new(
     name: 'languages',
     permissions: {
-      staff: 1
+      everyone: 1
     }
   )
 
+  tag_group.save
+else
+  tag_group.permissions = { everyone: 1 }
   tag_group.save
 end
 
