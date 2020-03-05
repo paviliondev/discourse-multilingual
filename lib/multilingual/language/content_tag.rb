22d6d9dccbcd84ea1b3c646c736bd83d6a473a74
Angus McLeod <angus@mcleod.org.au>
2020-03-06 09:10:13 +1100

    Update content_tag.rb

--- lib/multilingual/language/content_tag.rb
+++ lib/multilingual/language/content_tag.rb
@@ -106,7 +106,7 @@ class Multilingual::ContentTag
   def self.update_topic(topic, ctag_names = [])
     ctags = ctag_names.any? ? load(ctag_names) : []
     tags = topic.tags.select { |t| self.all.exclude?(t.name) }
-    topic.tags = tags + ctags 
+    topic.tags = (tags + ctags).uniq {|t| t.id } 
     topic.custom_fields['content_languages'] = ctags.any? ? ctags.map(&:name) : []
     topic
   end
