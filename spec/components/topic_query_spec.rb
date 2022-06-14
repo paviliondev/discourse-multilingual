bcbd8d30513720c7710d3bad4c34c07080cfd1eb
merefield <merefield@gmail.com>
2022-06-14 10:09:58 +0000

    IMPROVE: topic query test to add bespoke test for capitalised content tag

--- spec/components/topic_query_spec.rb
+++ spec/components/topic_query_spec.rb
@@ -15,6 +15,7 @@ describe TopicQuery do
   context "user has content languages" do
     fab!(:user1) { Fabricate(:user) }
     fab!(:user2) { Fabricate(:user) }
+    fab!(:user3) { Fabricate(:user) }
     fab!(:tag1) { Fabricate(:tag, name: "tag1") }
     fab!(:tag2) { Fabricate(:tag, name: "tag2") }
 
@@ -25,8 +26,13 @@ describe TopicQuery do
     }
 
     fab!(:language_topic2) {
-      language_tag2 = Tag.find_by(name: Multilingual::ContentTag.all.select { |t| t.include?("_") }.first)
+      language_tag2 = Tag.find_by(name: Multilingual::ContentTag.all.second)
       Fabricate(:topic, tags: [language_tag2])
+    }
+
+    fab!(:language_topic3) {
+      language_tag3 = Tag.find_by(name: Multilingual::ContentTag.all.select { |t| t.include?("_") && t.downcase != t }.first)
+      Fabricate(:topic, tags: [language_tag3])
     }
 
     fab!(:non_language_topic1) {
@@ -39,22 +45,32 @@ describe TopicQuery do
     }
 
     before do
-      user1.custom_fields['content_languages'] = [Multilingual::ContentTag.all.first, Multilingual::ContentTag.all.select { |t| t.include?("_") }.first]
+      user1.custom_fields['content_languages'] = [Multilingual::ContentTag.all.first, Multilingual::ContentTag.all.second]
       user1.save_custom_fields(true)
+      user3.custom_fields['content_languages'] = [Multilingual::ContentTag.all.first, Multilingual::ContentTag.all.second, Multilingual::ContentTag.all.select { |t| t.include?("_") && t.downcase != t }.first]
+      user3.save_custom_fields(true)
     end
 
-    it "filters topic list when content language topic filtering is enabled, whilst supporting capitalisation in content tags" do
+    it "filters topic list when content language topic filtering is enabled" do
       SiteSetting.multilingual_content_languages_topic_filtering_enabled = true
 
       expect(TopicQuery.new(user1).list_latest.topics.count).to eq(2)
-      expect(TopicQuery.new(user2).list_latest.topics.count).to eq(4)
+      expect(TopicQuery.new(user2).list_latest.topics.count).to eq(5)
     end
 
     it "does not filter topic list when content language topic filtering is disabled" do
       SiteSetting.multilingual_content_languages_topic_filtering_enabled = false
 
-      expect(TopicQuery.new(user1).list_latest.topics.count).to eq(4)
-      expect(TopicQuery.new(user2).list_latest.topics.count).to eq(4)
+      expect(TopicQuery.new(user1).list_latest.topics.count).to eq(5)
+      expect(TopicQuery.new(user2).list_latest.topics.count).to eq(5)
+      expect(TopicQuery.new(user2).list_latest.topics.count).to eq(5)
     end
+
+    it "filters topic list when content language topic filtering is enabled and includes content tags with capitalisation" do
+      SiteSetting.multilingual_content_languages_topic_filtering_enabled = true
+
+      expect(TopicQuery.new(user3).list_latest.topics.count).to eq(3)
+    end
+
   end
 end
