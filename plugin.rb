fb348c53c66645060df8bb6ea4bebaad37d5f4a4
Robert Barrow <merefield@gmail.com>
2022-07-08 07:56:54 +0100

    make version jump a little less dramatic

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-multilingual
 # about: Features to support multilingual forums
-# version: 1.0
+# version: 0.2.0
 # url: https://github.com/paviliondev/discourse-multilingual
 # authors: Angus McLeod, Robert Barrow
 
