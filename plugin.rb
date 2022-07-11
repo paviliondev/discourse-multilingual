79518af4e2be487e32ad9088d4838017a10f8779
merefield <merefield@gmail.com>
2022-07-11 20:46:02 +0100

    FIX: alter tagging reference

--- plugin.rb
+++ plugin.rb
@@ -166,7 +166,7 @@ after_initialize do
 
   add_class_method(:js_locale_helper, :output_locale_tags) do |locale_str|
     <<~JS
-      I18n.tag_translations = #{Multilingual::Translation.get("tag", locale_str).to_json};
+      I18n.tag_translations = #{Multilingual::Translation.get("tag").to_json[locale_str.to_sym]};
     JS
   end
 
@@ -228,7 +228,7 @@ after_initialize do
   add_to_serializer(:site, :categories) do
     object.categories.map do |c|
       c[:name] = c[:slug] == "uncategorized" ? I18n.t('uncategorized_category_name', locale: SiteSetting.default_locale) :
-      ((scope && scope.current_user ? Multilingual::Translation.get("category_name", c[:slug_path])[scope.current_user.locale.to_sym] :
+      ((scope && scope.current_user && c[:slug_path] ? Multilingual::Translation.get("category_name", c[:slug_path])[scope.current_user.locale.to_sym] :
       c[:name]) || c[:name])
       c.to_h
     end
