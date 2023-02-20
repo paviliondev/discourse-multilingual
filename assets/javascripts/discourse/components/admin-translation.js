12d28c14e0559a665ceb2f697f2ce8cbc8c206a1
merefield <merefield@gmail.com>
2023-02-20 20:01:34 +0000

    COMPATIBILITY: move admin translations to Glimmer

---
+++ assets/javascripts/discourse/components/admin-translation.js
@@ -0,0 +1,29 @@
+import MultilingualTranslation from "../models/multilingual-translation";
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class AdminTranslation extends Component {
+  @tracked removing = false;
+
+  @action
+  remove() {
+    this.removing = true;
+
+    MultilingualTranslation.remove(
+      this.args.translation.locale,
+      this.args.translation.file_type
+    ).then((result) => {
+      this.removing = false;
+      this.args.removed();
+    });
+  };
+
+  @action
+  download() {
+    MultilingualTranslation.download(
+      this.args.translation.locale,
+      this.args.translation.file_type
+    );
+  };
+};
